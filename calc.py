a1cc714f1d8e81cf5d29f6eae7c1e761fc10e3f2
Antons Pidgora <you@example.com>
2023-12-13 13:15:38 +0200

    gatavs

--- calc.py
+++ calc.py
@@ -1,24 +1,34 @@
 
 # konvertē datu mērvienības
 def megabyte_to_megabit(megabyte):
-   result = 0
+   result = megabyte * 8
    return result
 
 # saskaiti degvielas patēriņu uz 100 km
 def fuel_consumption(litres, kilometers):
-   result = 0
+
+   result = (100 / kilometers) * litres
    return result
 
 # konvertē temperatūru
 def celsius_to_fahrenheit(celsius):
-    result = 0
+    result = (celsius * (9/5)) + 32
     return result
 
 # saliec visus skaitļus pirms dota skaitļa (izmanto for)
 def sigma(tail):
     result = 0
+    for x in range(tail + 1):
+        result = result + x
     return result
 
 # nokonvertē svaru uz tuvāko mērvienību - grams, kilograms, tonna (izmanto if)
 def weight(grams):
-    return str(grams) + "g"
+    if (grams < 1000):
+        return str(grams) + "g"
+    elif (grams < 100000):
+        return str(int(grams / 1000)) + "kg"
+    elif (grams < 1000000):
+        return str(int(grams / 100000)) + "c"
+    else:
+        return str(int(grams / 1000000)) + "t"
